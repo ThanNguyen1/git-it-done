6fe6e74b548c5668568fcabf9d30902ba1692687
ThanNguyen1 <than.nnguyen@yahoo.com>
2021-12-14 16:52:57 -0600

    added issues list page and connected to homepage.html

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -55,34 +55,35 @@ var getUserRepos = function(user) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
   
     // create a container for each repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
   
-// create a span element to hold repository name
-var titleEl = document.createElement('span');
-titleEl.textContent = repoName;
+    // create a span element to hold repository name
+    var titleEl = document.createElement('span');
+    titleEl.textContent = repoName;
 
-// append to container
-repoEl.appendChild(titleEl);
+    // append to container
+    repoEl.appendChild(titleEl);
 
-// create a status element
-var statusEl = document.createElement('span');
-statusEl.classList = 'flex-row align-center';
+    // create a status element
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
 
-// check if current repo has issues or not
-if (repos[i].open_issues_count > 0) {
-  statusEl.innerHTML =
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+    statusEl.innerHTML =
     "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-} else {
-  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-}
+    } else {
+    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
 
-// append to container
-repoEl.appendChild(statusEl);
+    // append to container
+    repoEl.appendChild(statusEl);
 
-// append container to the dom
-repoContainerEl.appendChild(repoEl);
-}
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+    }
 };
 
   userFormEl.addEventListener("submit", formSubmitHandler);
